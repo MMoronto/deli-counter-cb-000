6460bed9744aa9913fe276e3447a9107d881aa6d
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-01-25 04:42:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+katz_deli = [] 
+
+def line(array)
+  if array.length == 0 
+    i = 0
+    puts "The line is currently empty."
+  else 
+    puts "The line is currently: #{[i + 1]}.  #{katz_deli[i]}"
+  end
+end 
